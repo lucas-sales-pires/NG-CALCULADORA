467593ebaa834181d6269320e8c5b0079bfb19b6
lucas-sales-pires <lucas2013sales@hotmail.com>
2024-04-26 06:48:17 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component, effect} from '@angular/core';
 import { setTheme } from 'ngx-bootstrap/utils';
 import { EstadoService } from './services/estado.service';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [CommonModule]
 })
 export class AppComponent {
   public titulo:string = "Angular Calculadora";
